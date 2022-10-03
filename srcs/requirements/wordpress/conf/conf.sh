9d1a606bdcf5ddf53953ad7c0e95380cd2876a0d
Amine <amine@JosMac-2.local>
2022-10-03 19:19:07 +0200

    Almost done

--- srcs/requirements/wordpress/conf/conf.sh
+++ srcs/requirements/wordpress/conf/conf.sh
@@ -6,7 +6,7 @@ wp config create --allow-root
 						--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
 wp core install --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-wp user create --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
+wp user create --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASSWORD --path='/var/www/wordpress' >> /log.txt
 
 if [ ! -d /run/php ]; then
 	mkdir ./run/php
